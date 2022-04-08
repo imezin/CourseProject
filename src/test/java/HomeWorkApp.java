15cee7fedd17d0c4b274a2f5045aba50ea7d95a0
imezin <Migor1298>
2022-04-08 16:10:26 +0300

    Initial commit

--- src/test/java/HomeWorkApp.java
+++ src/test/java/HomeWorkApp.java
@@ -1,10 +1,12 @@
 public class HomeWorkApp {
+
     public static void main(String[] args) {
         printThreeWords();
         checkSumSign();
         printColor();
         compareNumbers();
     }
+
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
@@ -20,6 +22,7 @@ public class HomeWorkApp {
             System.out.println("Сумма отрицательная");
         }
     }
+
     public static void printColor(){
         int value = 1;
         if (value <= 0){
@@ -30,6 +33,7 @@ public class HomeWorkApp {
             System.out.println("Зеленый");
         }
     }
+
     public static void compareNumbers(){
         int a = 122;
         int b = 12;
